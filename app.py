7c3ff23d84cc13a5191da27cc8f236be1072210a
tawandamavondo <tawandagdmavondo@gmail.com>
2022-04-10 12:19:29 +0200

    .

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ video_file = st.file_uploader(label="Video", type=['mp4'])
 
 if video_file is not None:
     active = True
-    # st.video(video_file.read(), format=video_file.type)
+    st.video(video_file.read(), format=video_file.type)
     # data_bytes = io.BytesIO(video_file.getvalue())
     # file_dir = temp_folder + "upload.mp4"
     # with open(file_dir, 'wb') as output:
